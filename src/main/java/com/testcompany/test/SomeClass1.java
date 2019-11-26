30ab94d1f31be18b4cba988067f0cc6183ceed79
llwantedl <llwantedl@gmail.com>
2019-11-26 12:57:56 +0200

    Project Init2

--- src/main/java/com/testcompany/test/SomeClass1.java
+++ src/main/java/com/testcompany/test/SomeClass1.java
@@ -2,6 +2,6 @@ package com.testcompany.test;
 
 public class SomeClass1 {
     public String someFunction1(String arg1, String arg2){
-        return arg1.concat(arg2);
+        return arg1.concat(arg2) + "f";
     }
 }
