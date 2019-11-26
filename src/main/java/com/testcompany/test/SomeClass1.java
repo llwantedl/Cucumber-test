ecf18d9a04cb341cabd3b4d81efea829391112c8
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-11-26 15:02:30 +0000

    Deploy llwantedl/Cucumber-test to github.com/llwantedl/Cucumber-test.git:gh-pages

--- src/main/java/com/testcompany/test/SomeClass1.java
+++ src/main/java/com/testcompany/test/SomeClass1.java
@@ -2,6 +2,10 @@ package com.testcompany.test;
 
 public class SomeClass1 {
     public String someFunction1(String arg1, String arg2){
-        return arg1.concat(arg2) + "f";
+        return arg1.concat(arg2);
+    }
+
+    public String someFailFunction2(String arg1, String arg2){
+        throw new RuntimeException();
     }
 }
